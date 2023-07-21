d823f903a43f5e223d02bd14ba8265a880a2d651
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2023-07-21 14:16:24 +0300

    Created, IBOutlets are connected.

---
+++ ByteCoinApp/Controller/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ByteCoinApp
+//
+//  Created by Ayça Işık on 21.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
